75feac07449507d8a1e85a902d3c9a1b5627e5b0
Ryan Stecker <voidedweasel@gmail.com>
2014-03-20 10:53:36 -0500

    Split monitors into separate files.

---
+++ StatusService/Monitor/MasterMonitor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusService
+{
+    class MasterMonitor : BaseMonitor
+    {
+        public MasterMonitor()
+            : base( null )
+        {
+        }
+
+        protected override void Tick()
+        {
+            base.Tick();
+        }
+    }
+}
