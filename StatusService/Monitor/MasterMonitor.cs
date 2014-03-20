edb8bc6cf2dc8abfb665ce7d8a5272b5c0449c18
Ryan Stecker <voidedweasel@gmail.com>
2014-03-20 10:56:32 -0500

    Periodically reconnect the master monitor to refresh the CM list.

--- StatusService/Monitor/MasterMonitor.cs
+++ StatusService/Monitor/MasterMonitor.cs
@@ -8,14 +8,34 @@ namespace StatusService
 {
     class MasterMonitor : BaseMonitor
     {
+        DateTime nextRelog = DateTime.MaxValue;
+
+
         public MasterMonitor()
             : base( null )
         {
         }
 
+        protected override void OnLoggedOn( SteamKit2.SteamUser.LoggedOnCallback callback )
+        {
+            nextRelog = DateTime.Now + TimeSpan.FromMinutes( 30 );
+
+            base.OnLoggedOn( callback );
+        }
+
         protected override void Tick()
         {
             base.Tick();
+
+            if ( DateTime.Now >= nextRelog )
+            {
+                if ( Client.IsConnected )
+                {
+                    Client.Disconnect();
+                }
+
+                nextRelog = DateTime.Now + TimeSpan.FromMinutes( 30 );
+            }
         }
     }
 }
